4df7cf5020ba55f8f45526b2af2cc8377df8e5e5
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-05-26 16:12:22 +0200

    "Fix formatting and typo issues in UserCard component."

--- components/UserCard.tsx
+++ components/UserCard.tsx
@@ -1,12 +1,12 @@
 import {ThemedText} from "@/components/ThemedText";
 import {User} from "@/models/User";
 
-type UserCardProps = {user:User}
+type UserCardProps = { user: User }
 
-export function UserCard({user}: {user : UserCardProps}){
-        return(
-            <ThemedText>
+export function UserCard({user}: { user: UserCardProps }) {
+    return (
+        <ThemedText>
             {UserCard.name}
         </ThemedText>
-        );
+    );
 }
