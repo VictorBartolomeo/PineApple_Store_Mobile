441c2f44c43f3d24312e6480c319b8186cc527c0
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-05-26 09:53:59 +0200

    Refactor TestComponent to include button styling, user data fetching, and click tracking functionality.

--- components/TestComponent.tsx
+++ components/TestComponent.tsx
@@ -1,16 +1,49 @@
-import {GestureResponderEvent, Pressable} from 'react-native';
-import {ThemedView} from "@/components/ThemedView";
+import {GestureResponderEvent, Pressable, StyleSheet} from 'react-native';
 import {ThemedText} from "@/components/ThemedText";
+import {useEffect, useState} from "react";
 
-type TestComponentProps= {value:string, callback : (event:GestureResponderEvent) =>void }
+type TestComponentProps = { value: string, callback: (event: GestureResponderEvent) => void }
 
-export function TestComponent({value, callback}: TestComponentProps){
+export function TestComponent() {
 
-  return(
-      <Pressable onPress={callback}>
-        <ThemedView>
-          <ThemedText>MON TEST : {value}</ThemedText>
-        </ThemedView>;
-      </Pressable>
-    )
+    const [numberClick, setNumberClick] = useState(0)
+    let userList = {name: string}[] = []
+
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then(res => res.json())
+            .then(users => userList = users.map(
+                (user: {name : string}) =>
+                <ThemedText>{user.name}</ThemedText>))
+    }, [numberClick]);
+
+    const userNameList = []
+
+
+    return <>
+        <Pressable onPress={() => {
+            setNumberClick(numberClick + 1)
+            console.log(numberClick)
+        }}>
+            <ThemedText style={styles.button}>Press me !</ThemedText>
+        </Pressable>
+        <ThemedText>{numberClick}</ThemedText>
+    </>
 }
+
+const styles = StyleSheet.create({
+    button: {
+        color: "white",
+        backgroundColor: "blue",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 10,
+        borderStyle: "solid",
+        borderWidth: 2,
+        borderColor: "white",
+        borderRadius: 10,
+        margin: 70,
+        shadowColor: "white"
+    }
+})
