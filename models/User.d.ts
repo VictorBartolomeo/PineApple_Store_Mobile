50e0529fa53041898a81d9278ab27c425321c2e1
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-05-28 11:34:26 +0200

    Add extended User type definition with optional fields.

--- models/User.d.ts
+++ models/User.d.ts
@@ -1,3 +1,24 @@
+// models/User.d.ts
 export type User = {
+    id: number;
     name: string;
-}
+    username: string;
+    email: string;
+    address?: { // Les champs optionnels sont marqués avec ?
+        street: string;
+        suite: string;
+        city: string;
+        zipcode: string;
+        geo?: {
+            lat: string;
+            lng: string;
+        };
+    };
+    phone?: string;
+    website?: string;
+    company?: {
+        name: string;
+        catchPhrase?: string;
+        bs?: string;
+    };
+};
